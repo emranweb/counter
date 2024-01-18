a442947f582805e380a96dcc506f1128926e15bc
emranweb <emranbd77@gmail.com>
2024-01-18 21:37:35 +0600

    update navbar

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -1,48 +1,103 @@
 "use client";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-import navItems from "@/app/data/navItems.json";
 
 const Navbar = () => {
-    const computerCategory = navItems.slice(0, 9);
-    const fruitsCategory = navItems.slice(10);
-    const [activeCategory, setActiveCategory] = React.useState("computer");
-
     return (
-        <div className="absolute left-0 top-0 bg-slate-200 w-32 text-black h-full">
-            <div className="flex flex-col gap-4">
-                <div className="">
-                    <h3
-                        className="text-2xl font-bold"
-                        onClick={() => setActiveCategory("computer")}
-                    >
-                        Computer
-                    </h3>
-                    <div className="flex flex-col gap-4">
-                        {activeCategory === "computer"
-                            ? computerCategory.map((item) => (
-                                  <span key={item.id}>{item.name}</span>
-                              ))
-                            : ""}
-                    </div>
-                </div>
+        <nav className="bg-white shadow-lg">
+            <div className="max-w-6xl mx-auto px-4">
+                <div className="flex justify-between">
+                    <div className="flex space-x-7 justify-between w-full">
+                        <div>
+                            <Link href="/" className="items-center py-4 px-2">
+                                <Image
+                                    width={50}
+                                    height={50}
+                                    src="/logo.png"
+                                    alt="Logo"
+                                />
+                            </Link>
+                        </div>
 
-                <div className="">
-                    <h3
-                        className="text-2xl font-bold"
-                        onClick={() => setActiveCategory("fruit")}
-                    >
-                        Fruit
-                    </h3>
-                    <div className="flex flex-col gap-4 transition-all ease-in duration-300">
-                        {activeCategory === "fruit"
-                            ? fruitsCategory.map((item) => (
-                                  <span key={item.id}>{item.name}</span>
-                              ))
-                            : ""}
+                        <div className="hidden md:flex items-center space-x-1">
+                            <a
+                                href=""
+                                className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
+                            >
+                                Couter
+                            </a>
+                            <a
+                                href=""
+                                className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                            >
+                                Expense
+                            </a>
+                            <a
+                                href=""
+                                className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+                            >
+                                User
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="md:hidden flex items-center">
+                        <button className="outline-none mobile-menu-button">
+                            <svg
+                                className=" w-6 h-6 text-gray-500 hover:text-green-500 "
+                                x-show="!showMenu"
+                                fill="none"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path d="M4 6h16M4 12h16m-7 6h7"></path>
+                            </svg>
+                        </button>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div className="hidden mobile-menu">
+                <ul className="">
+                    <li>
+                        <a
+                            href="index.html"
+                            className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#services"
+                            className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                        >
+                            Services
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#about"
+                            className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                        >
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="#contact"
+                            className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
+                        >
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 };
 
