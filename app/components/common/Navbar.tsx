6386f3d8d9e1c52157032f2b49184f86ba53b768
emranweb <emranbd77@gmail.com>
2024-01-19 12:17:09 +0600

    update link

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Navbar = () => {
     return (
         <nav className="bg-white shadow-lg">
-            <div className="max-w-6xl mx-auto px-4">
+            <div className="mx-auto px-4">
                 <div className="flex justify-between">
                     <div className="flex space-x-7 justify-between w-full">
                         <div>
@@ -21,24 +21,19 @@ const Navbar = () => {
                         </div>
 
                         <div className="hidden md:flex items-center space-x-1">
-                            <a
-                                href=""
+                            <Link
+                                href="/coffee"
                                 className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold "
                             >
                                 Couter
-                            </a>
-                            <a
-                                href=""
-                                className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-                            >
-                                Expense
-                            </a>
-                            <a
-                                href=""
+                            </Link>
+
+                            <Link
+                                href="/user"
                                 className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
                             >
                                 User
-                            </a>
+                            </Link>
                         </div>
                     </div>
 
