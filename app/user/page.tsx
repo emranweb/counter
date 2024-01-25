848f4520c8e966608e08b1a40a620c1b3f8ea51d
emranweb <emranbd77@gmail.com>
2024-01-25 12:07:37 +0600

    update the page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 
 const UserPage = () => {
-    return <div>UserPage</div>;
+    const [isRequest, setIsRequest] = useState(false);
+    return (
+        <div>
+            <h1>User Profile</h1>
+        </div>
+    );
 };
 
 export default UserPage;
