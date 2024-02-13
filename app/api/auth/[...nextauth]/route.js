b38c506393311b9da6e49b14df0257c62be7e52b
emranweb <emranbd77@gmail.com>
2024-02-13 11:36:15 +0600

    working issue fixed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,12 +1,23 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 const handler = NextAuth({
+    pages: {
+        signIn: "/login",
+    },
     providers: [
         CredentialsProvider({
             name: "Credentials",
             credentials: {
                 email: { type: "email" },
                 password: { type: "password" },
+            },
+            async authorize(credentials, req) {
+                const user = { id: 1, email: "abc@gmail.com", password: "123" };
+                if (user) {
+                    return user;
+                } else {
+                    return null;
+                }
             },
         }),
     ],
