67f7d32f570e36b93920d7799a07e859784dc3ba
emranweb <emranbd77@gmail.com>
2024-03-13 07:34:48 +0600

    update middlware and login

--- app/api/users/verify/router.ts
+++ app/api/users/verify/router.ts
@@ -1,10 +1,14 @@
 import { connection } from "@/app/database/dbconnect";
 
 export async function POST(request: Request, response: Response) {
+    console.log("data");
     const data = await request.body;
+    console.log(data);
+
     const [results] = await connection.query(
-        "SELECT * FROM users WHERE email = ? AND pasword= ?",
-        ["test@gmail.com", "122"]
+        "SELECT * FROM users WHERE email = ?",
+        ["jane.doe@example.com"]
     );
+    console.log(results);
     return Response.json(results);
 }
