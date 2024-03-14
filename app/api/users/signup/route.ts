c5e029c4d6bf02f7e96ee524b0e6a1730e19d4a0
emranweb <emranbd77@gmail.com>
2024-03-14 07:54:18 +0600

    add login and register

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,11 +1,19 @@
 import { connection } from "@/app/database/dbconnect";
+import bcrypt from "bcrypt";
+
+const generatePasword = (password: string) => {
+    const data = process.env.NEXT_BSCRYPT_SALT;
+    const salt = bcrypt.genSaltSync(Number(data));
+    return bcrypt.hashSync(password, salt);
+};
 
 export async function POST(req: Request, res: Response) {
     const data = await req.json();
+    const hashPassword = generatePasword(data.password);
     if (data) {
         const [results] = await connection.query(
-            "INSERT INTO users (fullName, email, password) VALUES (?, ?, ?)",
-            [data.fullName, data.email, data.password]
+            "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+            [data.fullName, data.email, hashPassword]
         );
         return Response.json(results);
     } else {
--- app/utils/utils.ts
+++ app/utils/utils.ts
@@ -7,3 +7,20 @@ export function convertToClientDateTime(databaseDateTimeString = "") {
     const localDateTime = new Date(dateTime.getTime() - offsetMilliseconds);
     return localDateTime.toLocaleString();
 }
+
+// Generated a hashed pasword
+
+// export const generatePasword = (password: string) => {
+//     const data = process.env.NEXT_BSCRYPT_SALT;
+//     const salt = bcrypt.genSaltSync(Number(data));
+//     return bcrypt.hashSync(password, salt);
+// };
+
+// Compare the password
+
+// export const comparePassword = (
+//     currentPassword: string,
+//     hashPassword: string
+// ) => {
+//     return bcrypt.compareSync(currentPassword, hashPassword);
+// };
